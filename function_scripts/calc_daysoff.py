442bdca135be47f572dbc65df07bea77876f5470
david-devito <ddevito1988@gmail.com>
2021-02-13 22:33:58 -0500

    Added calculation of whether game follows a day off or not

---
+++ function_scripts/calc_daysoff.py
@@ -0,0 +1,35 @@
+# LOAD GAME LOGS CSVS AND ADD COLUMNS WITH NUMBER OF DAYS OFF BEFORE CURRENT GAME FOR HOME AND AWAY TEAM
+
+import sys
+
+sys.path.append('../input/gamelogs')
+
+
+import pandas as pd
+import numpy as np
+from datetime import datetime, timedelta
+
+yeari = 2016
+statsDF = pd.read_csv('../input/gamelogs/gamelogs' + str(yeari) + '_orig.csv', sep=',')
+
+statsDF['Date_dt'] = statsDF['Date'].apply(lambda x: datetime.strptime(x, '%d-%m-%Y'))
+
+APlayedYest = []
+HPlayedYest = []
+for i in statsDF.index:
+    dayBefore = statsDF.loc[i]['Date_dt'] - timedelta(1)
+    curATeam = statsDF.loc[i]['AwayTeam']
+    curHTeam = statsDF.loc[i]['HomeTeam']
+    
+    APlayedBefore = statsDF[((statsDF['AwayTeam'] == curATeam) | (statsDF['HomeTeam'] == curATeam)) & (statsDF['Date_dt'] == dayBefore)]
+    if len(APlayedBefore) > 0: APlayedYest.append(1)
+    else: APlayedYest.append(0)
+    HPlayedBefore = statsDF[((statsDF['AwayTeam'] == curHTeam) | (statsDF['HomeTeam'] == curHTeam)) & (statsDF['Date_dt'] == dayBefore)]
+    if len(HPlayedBefore) > 0: HPlayedYest.append(1)
+    else: HPlayedYest.append(0)
+    
+statsDF['APlayedYest'] = APlayedYest
+statsDF['HPlayedYest'] = HPlayedYest
+
+
+statsDF.to_csv('../input/gamelogs/gamelogs' + str(yeari) + '.csv', index=False)
